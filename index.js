e7bdef569752992d64f326735945f3225d9ff15c
Aniket Jha <84675061+aniketjha9179@users.noreply.github.com>
2023-07-14 17:21:53 +0530

    server_creation in node js
    creating a simple server

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
-// let app  = require('./app')
-// var x = '20'
-// const arr = [1,2,4,5,5,6,6];
-// // console.log(arr[0]);
-// // console.log(app.z());
+const http = require("http");
 
 
 
-//  let result = arr.filter((item)=>{
-// return item>2
+ let dataControl =(res,req)=>{
+    res.write("<h1>JAY SHREE RAM</h1>");
 
-// })
-// console.log(result);
+    res.end();
+}
 
 
-let keke = require('fs') ;
-keke.writeFileSync('file.txt', 'HEloo how are you ')
-// console.log('->>',__dirname     );
-// console.log('->>',__filename     );
+
+
+http
+  .createServer(dataControl)
+  .listen(4500);
