9f766bcd557d1c4a785b35b40f6df7059afe45ad
aniketjha9179 <aniketjha917928@gmail.com>
2023-07-14 11:03:06 +0530

    node js begginers

---
+++ index.js
@@ -0,0 +1,19 @@
+// let app  = require('./app')
+// var x = '20'
+// const arr = [1,2,4,5,5,6,6];
+// // console.log(arr[0]);
+// // console.log(app.z());
+
+
+
+//  let result = arr.filter((item)=>{
+// return item>2
+
+// })
+// console.log(result);
+
+
+let keke = require('fs') ;
+keke.writeFileSync('file.txt', 'HEloo how are you ')
+// console.log('->>',__dirname     );
+// console.log('->>',__filename     );
