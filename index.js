559f7e8255c66af5d8c4f77712bc9865663c30bb
aniketjha9179 <aniketjha917928@gmail.com>
2023-07-16 13:00:05 +0530

    first api in next js with nodemon runnning

--- index.js
+++ index.js
@@ -1,19 +1,9 @@
-// let app  = require('./app')
-// var x = '20'
-// const arr = [1,2,4,5,5,6,6];
-// // console.log(arr[0]);
-// // console.log(app.z());
-
-
-
-//  let result = arr.filter((item)=>{
-// return item>2
-
-// })
-// console.log(result);
-
-
-let keke = require('fs') ;
-keke.writeFileSync('file.txt', 'HEloo how are you ')
-// console.log('->>',__dirname     );
-// console.log('->>',__filename     );
+const http = require("http");
+const data = require("./data");
+http
+  .createServer((req, res) => {
+    res.writeHead(200, { "Content-Type ": "application/json" });
+    res.write(JSON.stringify(data));
+    res.end();
+  })
+  .listen(4500);
