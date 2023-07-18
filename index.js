2aac3810a70897025f6c50c1ee405cd5154122a2
aniketjha9179 <aniketjha917928@gmail.com>
2023-07-18 17:35:46 +0530

    express-overview

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
-let a = 20 ;
-let b =0 ;
+const express = require("express");
+const app = express();
 
+app.get("", (req, res) => {
+  console.log('data send by browser-------- b>', req.query.name);
+  res.send(" welcome , hello , this is Home Page");
+});
 
-let waitingData =  new Promise((resolve, reject)=>{
-    setTimeout(()=>{
-        resolve(10)
-    },2000)
-    
-})
-waitingData.then((b)=>{
+app.get("/about", (req, res) => {
+    res.send("hello , this is About Page");
+  });
+  app.get("/help", (req, res) => {
+    res.send({
+      name:'aniket',
+      email:'ani@gmail.com'
+    });
+  });
+  app.get("/wedss", (req, res) => {
+    res.send("hello , this is weds Page");
+  });
 
-    console.log(a+b);
-
-})
+  app.listen(5000)
