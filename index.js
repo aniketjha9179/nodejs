a8a5f0df32a00ee8c1df5436c5b36ea37eaedc70
aniketjha9179 <aniketjha917928@gmail.com>
2023-07-20 11:24:15 +0530

    ejs_file_init

--- index.js
+++ index.js
@@ -1,25 +1,40 @@
-const path = require('path')
+const path = require("path");
 
 const express = require("express");
 
 const app = express();
 
-const publicPath = path.join(__dirname, "public")
+const publicPath = path.join(__dirname, "public");
 
+app.set("view engine", "ejs");
 
-// app.use(express.static(publicPath))
-app.get('',(_r,res)=>{
-res.sendFile(`${publicPath}/home.html`)
+app.get("", (_r, res) => {
+  res.sendFile(`${publicPath}/home.html`);
+});
+
+app.get("/profile", (_r, res) => {
+  const user = {
+    name: "aniket",
+    city: "bhopal",
+    email: "aj@gmail.com ",
+    skills: ["php", "js", "nodejs", "java", "cpp", "scala"],
+  };
+
+  res.render("profile", { user });
+});
+
+app.get('/login',(_r,res)=>{
+  res.render('login');
 })
 
-app.get('/about',(_r,res)=>{
-  res.sendFile(`${publicPath}/about.html`)
-  })
-  app.get('/help',(_r,res)=>{
-    res.sendFile(`${publicPath}/help.html`)
-    })
+app.get("/about", (_r, res) => {
+  res.sendFile(`${publicPath}/about.html`);
+});
+app.get("/help", (_r, res) => {
+  res.sendFile(`${publicPath}/help.html`);
+});
 
-    app.get('*',(_r,res)=>{
-      res.sendFile(`${publicPath}/nopage.html`)
-      })
+app.get("*", (_r, res) => {
+  res.sendFile(`${publicPath}/nopage.html`);
+});
 app.listen(5000);
