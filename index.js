c7a9a6677fa03806bdc40166efdb5829e1b9f572
aniketjha9179 <aniketjha917928@gmail.com>
2023-07-21 17:48:02 +0530

    reading mongodb data in nodejs console

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-// connection mongo db with node
+const dbConnect = require('./mongodb')
+dbConnect().then((res)=>{  
+                        //.toarray return Promise
+  res.find({}).toArray().then((data)=>{
+    // console.log(data)
 
-const { MongoClient } = require("mongodb");
-const database = "e-commerce";
-const url = "mongodb://127.0.0.1:27017";
+  })
+})
 
-const client = new MongoClient(url);
 
-async function getData() {
-  let result = await client.connect();
-  let db = result.db(database);
-  //telling which table to connect of mongodb
-  let collection = db.collection("products");
-  let response = await collection.find({}).toArray();
-  console.log(response);
-}
+// console.log(dbConnect());
 
-getData();
+ const main = async()=>{
+  let data = await dbConnect()
+  data = await data.find().toArray()
+  console.log(data);
+
+ }
+ main()
