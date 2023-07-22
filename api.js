658c30ddce0dc800d8871b05b67e50641a74f10d
aniketjha9179 <aniketjha917928@gmail.com>
2023-07-22 21:29:46 +0530

    deleting the data from delete method in node

--- api.js
+++ api.js
@@ -1,12 +1,40 @@
 const express = require("express");
 const dbConnect = require("./mongodb");
 const app = express();
+const mongodb = require('mongodb') 
+
+app.use(express.json());
 
 app.get("/", async (req, res) => {
   let data = await dbConnect();
   data = await data.find().toArray();
-  console.log(data);
-
   res.send(data);
+  //   console.log(data);
+  //
 });
+app.post("/", async (req, res) => {
+  // console.log(req.body);
+  let data = await dbConnect();
+  let result =await data.insertOne(req.body).
+  res.send(result);
+});
+
+
+app.put('/',async (req, res)=>{
+let data = await dbConnect()
+let result= data.updateOne(
+  {name:req.body.name},
+  {$set:req.body})
+  res.send({result:'updated'});
+
+})
+app.delete('/:id', async(req,res)=>{
+  console.log(req.params.id);
+  const data =await dbConnect()
+  const result=await data.deleteOne({_id:new mongodb.ObjectId(req.params.id)})
+  
+  res.send(result)
+})
+
+
 app.listen(5000);
