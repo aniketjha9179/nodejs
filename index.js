176b39651caa0e4fc19af633bd9d9e3d1ceff022
aniketjha9179 <aniketjha917928@gmail.com>
2023-07-23 13:36:43 +0530

    file upload using multer-in node js

--- index.js
+++ index.js
@@ -1,58 +1,19 @@
-const mongoose = require("mongoose");
+const express = require("express");
+const multer = require("multer");
+const app = express();
 
-// Define the ProductSchema outside of the functions
-const ProductSchema = new mongoose.Schema({
-  name: String,
-  price: Number,
-  brand: String,
-  category: String,
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "uploads");
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.fieldname + "_" + Date.now() + ".jpg");
+    },
+  }),
+}).single('user_file')
+
+app.post("/upload",upload, (req, res) => {
+  res.send("file uploaded");
 });
-
-const saveInDB = async () => {
-  await mongoose.connect("mongodb://127.0.0.1:27017/e-commerce");
-  const ProductsModel = mongoose.model('products', ProductSchema)
-  let data = new ProductsModel({
-    name: 'm18',
-    price: 1200,
-    brand: 'apple',
-    category: 'smart-phone',
-  });
-  let result = await data.save();
-  console.log(result);
-};
-
-// saveInDB();
-
-
-const updateInDB = async () => {
-  // Use the ProductSchema here
-  const Product = mongoose.model('products', ProductSchema);
-  let data = await Product.updateOne(
-    { name: 'nord3' },
-    {
-      $set: { price: 10, name:'alu paratha' },
-    }
-  );
-  console.log(data);
-};
-
-// updateInDB();
-
-
-const deleteInDB = async()=>{
-  const Product = mongoose.model('products', ProductSchema);
-  let data = await Product.deleteOne({name:'iphone15'})
-  console.log(data);
-
-
-}
-// deleteInDB()
-
-
-const FindInDB = async()=>{
-  const Product = mongoose.model('products', ProductSchema);
-  let data = await Product.find({name:'m18'})
-  console.log(data);
-}
-FindInDB()
-
+app.listen(5000);
