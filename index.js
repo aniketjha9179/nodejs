731b688f07eace00f1b885daff278378aa7288a7
aniketjha9179 <aniketjha917928@gmail.com>
2023-07-23 09:26:01 +0530

    added schema*model to filter the type of data in db

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-const dbConnect = require('./mongodb')
-dbConnect().then((res)=>{  
-                        //.toarray return Promise
-  res.find({}).toArray().then((data)=>{
-    // console.log(data)
+const mongoose = require("mongoose");
 
+const main = async () => {
+  await mongoose.connect("mongodb://127.0.0.1:27017/e-commerce");
+  const ProductSchema = new mongoose.Schema({
+    name: String,
+    price:Number,
+
+  });
+  const ProductsModel =  mongoose.model('products',ProductSchema)
+  let data = new ProductsModel({
+    name:'m18',
+    price:1000
   })
-})
+  let result  = await data.save()
+  console.log(result);
+};
+main()
 
-
-// console.log(dbConnect());
-
- const main = async()=>{
-  let data = await dbConnect()
-  data = await data.find().toArray()
-  console.log(data);
-
- }
- main()
